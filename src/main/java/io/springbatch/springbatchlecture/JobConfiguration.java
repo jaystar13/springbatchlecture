f51684f27cd26a574cfc380fff1c523beeaaf99f
lee.jaesung <lee.jaesung@joins.com>
2022-04-25 07:57:30 +0900

    스프링 배치 도메인 이해 - Job End

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -9,15 +9,15 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@RequiredArgsConstructor
 @Configuration
-public class DBJobConfiguration {
+@RequiredArgsConstructor
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloJob() {
+    public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
@@ -25,24 +25,22 @@ public class DBJobConfiguration {
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println(" >> step2 was executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build()
-                ;
-    }
-
-    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println(" >> step1 was executed");
+                    System.out.println("step1 was executed");
                     return RepeatStatus.FINISHED;
                 })
-                .build()
-                ;
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 }
